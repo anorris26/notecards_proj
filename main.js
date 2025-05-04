69312eeaea19f10cbd677f1560f0df2ce8362b40
Anna Norris <alnorris113@gmail.com>
2025-05-03 17:49:18 -0700

    added another input field

--- main.js
+++ main.js
@@ -2,27 +2,35 @@ let inum = localStorage.length;
 let wordind = document.getElementById("testList");
 
 document.getElementById("test").onclick = function() {
-    const x = document.getElementById("inp").value;
-    localStorage.setItem(`${inum}`, x);
+    let answer = document.getElementById("answer").value;
+    let question = document.getElementById("question").value
+    localStorage.setItem(answer, question);
 
-    document.getElementById("demo").innerHTML = `new element: ${x}`;
+    document.getElementById("demo").innerHTML = `new element: ${answer} and ${question}`;
     const listItem = document.createElement("li");
-    listItem.textContent = x;
+    listItem.textContent = answer + ": " + question;
     wordind.appendChild(listItem);
 
+    document.getElementById("answer").value = "";
+    document.getElementById("question").value = "";
     inum += 1;
 }
 
 document.getElementById("delete").onclick = function() {
     localStorage.clear();
-    
+    document.getElementById("demo").innerHTML = "";
+    wordind.textContent = "";
+
 }
 
 window.onload = function() {
+    document.getElementById("answer").value = "";
+    document.getElementById("question").value = "";
     for (let i = 0; i < localStorage.length; i++) {
         const item = localStorage.getItem(localStorage.key(i));
+        console.log(localStorage.getItem(localStorage.key(i)));
         const listItem = document.createElement("li");
-        listItem.textContent = item;
+        listItem.textContent = localStorage.key(i) + ": " + item;
         wordind.appendChild(listItem);
     }
 }
